a3ca7d34611a83526c1258a3eefa550d10683ccd
jingguohua <jingguohua@jxzm.net>
2026-09-20 00:32:28 +0800

    fix(d1): 降低事件写入放大率

---
+++ cloudflare/migrations/0043_drop_heavy_read_write_amplification_indexes.sql
@@ -0,0 +1,3 @@
+DROP INDEX IF EXISTS idx_events_app_type_name_ts;
+DROP INDEX IF EXISTS idx_events_app_type_metric_ts;
+DROP INDEX IF EXISTS idx_events_app_release_type_metric_ts;
---
+++ cloudflare/migrations/0044_drop_secondary_events_write_amplification_indexes.sql
@@ -0,0 +1,2 @@
+DROP INDEX IF EXISTS idx_events_app_release_ts;
+DROP INDEX IF EXISTS idx_events_type_name_ts;
